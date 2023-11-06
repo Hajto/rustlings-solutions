2bf4a29192d700816def7069166422c1c3e2d2e9
Haito <hajtosek@gmail.com>
2023-11-06 18:47:27 +0100

    Day 1

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -34,6 +32,11 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+        add_scores(&mut scores, team_1_name, team_1_score, team_2_score);
+        add_scores(&mut scores, team_2_name, team_2_score, team_1_score);
+
+
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
@@ -41,6 +44,13 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // team_1.
     }
     scores
+}
+
+
+fn add_scores(scores: &mut HashMap<String, Team>, team_name: String, scored: u8, conceded: u8) -> () {
+    let team =scores.entry(team_name).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+    (* team).goals_scored += scored;
+    (* team).goals_conceded += conceded;
 }
 
 #[cfg(test)]
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_of_apples: i32) -> i32 {
+    let rustbucks_multiplier = if num_of_apples > 40 {
+        1
+    } else {
+        2
+    };
+
+    num_of_apples * rustbucks_multiplier
+}
 
 // Don't modify this function!
 #[test]
